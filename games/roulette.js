95fcf7a6a9709f7a2a0f12a9ef279a2fc82fb682
szymonjankowski1332-bit <szymonjankowski1332@gmail.com>
2026-04-03 13:40:49 +0200

    Update roulette.js

--- games/roulette.js
+++ games/roulette.js
@@ -1,1 +1,16 @@
+function loadRoulette(){
+let el=document.getElementById("gameScreen");
 
+el.innerHTML=`
+<h2>Ruletka</h2>
+<div id="wheel"></div>
+<button onclick="spinRoulette()">SPIN</button>
+<button onclick="show('lobby')">⬅️</button>
+<p id="result"></p>
+`;
+
+window.spinRoulette=function(){
+let win=Math.floor(Math.random()*37);
+document.getElementById("result").innerText="Wynik: "+win;
+};
+}
