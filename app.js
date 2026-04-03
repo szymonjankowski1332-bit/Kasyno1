c2f8e2b34499f86d0557360c6df098665c738c29
szymonjankowski1332-bit <szymonjankowski1332@gmail.com>
2026-04-03 13:39:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+let users=JSON.parse(localStorage.getItem("users")||"{}");
+let currentUser="";
 
+function login(){
+currentUser=document.getElementById("nick").value;
+if(!users[currentUser]) users[currentUser]=1000;
+
+show("lobby");
+updateBalance();
+}
+
+function show(id){
+document.querySelectorAll(".screen").forEach(x=>x.classList.remove("active"));
+document.getElementById(id).classList.add("active");
+}
+
+function updateBalance(){
+document.getElementById("bal").innerText=users[currentUser];
+localStorage.setItem("users",JSON.stringify(users));
+}
+
+function openGame(name){
+show("gameScreen");
+
+if(name==="roulette") loadRoulette();
+if(name==="slots") loadSlots();
+if(name==="aviator") loadAviator();
+}
