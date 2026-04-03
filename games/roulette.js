f5b5ca2d3320477ab33fa47d78b096d97d4565c2
szymonjankowski1332-bit <szymonjankowski1332@gmail.com>
2026-04-03 13:55:12 +0200

    Update roulette.js

--- games/roulette.js
+++ games/roulette.js
@@ -134,3 +134,17 @@ createTable();
 
 },3000);
 }
+<div id="numbers"></div>
+function drawNumbers(){
+let nums=[0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10,5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26];
+
+let container=document.getElementById("numbers");
+container.innerHTML="";
+
+nums.forEach(n=>{
+let d=document.createElement("span");
+d.innerText=n+" ";
+container.appendChild(d);
+});
+}
+drawNumbers();
